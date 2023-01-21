55a9ea59ba6d98240542826b45643b7dbd97f5a0
Keith Brinks <keith@brinks.me>
2023-01-21 09:42:50 -0500

    Show elapsed time

--- assets/js/km.js
+++ assets/js/km.js
@@ -1,5 +1,9 @@
 jQuery(function($) {
-	$('#countdown').countdown('2016/04/09', function(event) {
-		$(this).html(event.strftime('%D days'));
+	$('#countdown').countdown('2016/04/09', {elapse: true}).on('update.countdown', function(event) {
+		if (event.elapsed) {
+			$(this).html(event.strftime('Married for %D days!'));
+		} else {
+			$(this).html(event.strftime('%D days'));
+		}
 	});
 });
